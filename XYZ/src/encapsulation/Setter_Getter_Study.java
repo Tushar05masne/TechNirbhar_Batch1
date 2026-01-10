34d75e56eef2ea361336fe244fc0b51a92970572
Tushar05masne <masnetushar05@gmail.com>
2026-01-10 21:13:18 +0530

    Pending code 2

---
+++ XYZ/src/encapsulation/Setter_Getter_Study.java
@@ -0,0 +1,20 @@
+package encapsulation;
+
+public class Setter_Getter_Study {
+
+	public static  String var="Name of cust is TOM";
+
+	
+	
+	
+	public static void SetVaraible(String varaible) {
+		
+		var= varaible;
+	}
+    public static String GetVariable() {
+    	
+    	return var;
+    }
+    
+    
+}
